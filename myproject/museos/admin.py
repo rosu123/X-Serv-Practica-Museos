d001052196cbf3da1d85cd36000932148f2d2198
Rodrigo Perela Posada <rperela@alpha30.aulas.gsyc.urjc.es>
2018-04-27 16:29:18 +0200

    añadido esbozo del models

---
+++ myproject/museos/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from museos.models import Museo
+from museos.models import Seleccion
+from museos.models import Comentario
+from museos.models import Configuracion
+
+admin.site.register(Museo)
+admin.site.register(Seleccion)
+admin.site.register(Comentario)
+admin.site.register(Configuracion)
---
+++ myproject/museos/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Museo(models.Model):
+    idEntidad = models.IntegerField()
+    nombre = models.CharField(max_length=64)
+    descripcion = models.TextField()
+    horario = models.TextField()
+    transporte = models.TextField()
+    accesibilidad = models.BinaryField()
+    contentURL = models.URLField()
+    distrito = models.CharField(max_length=32)
+    telefono = models.TextField()
+    email = EmailField()
+
+    #def __str__(self):
+        #return self.name
+
+
+class Seleccion(models.Model):
+    museo = models.ForeignKey(Museo)
+    user = models.CharField(max_length=32)
+
+
+class Comentario(models.Model):
+    museo = models.ForeignKey(Museo)
+    texto = models.TextField()
+    fecha = DateTimeField(auto_now=True)
+
+    #def __str__(self):
+        #return self.grupo.nombre + " " + str(self.fecha)
+
+
+class Configuracion(models.Model):
+    user = models.CharField(max_length=32)
+    titulo_pag = models.CharField(max_length=64)
+    tamano = models.IntegerField()
+    letra = models.CharField(max_length=64)
+    color = models.CharField(max_length=32)
